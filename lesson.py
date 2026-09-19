a73370ea6b2ae65e399d4b62a48d36690fa81d35
Medya Dağlı <medyadagli9@gmail.com>
2026-09-19 14:06:59 +0300

    Add prime numbers program

--- lesson.py
+++ lesson.py
@@ -1,16 +1,15 @@
 input_number=int(input("Enter a number:"))
-isPrime=True
-if(input_number<0):
-    print("Entered a negative number. Enter a positive number")
-elif(input_number>0 and input_number<2):
- print("The smallest prime number is two.")
+prime_numbers=[]
+if input_number<0:
+    print("Entered a negative number.Enter positive a number.")
+elif input_number>0 and input_number<2:
+    print("The smallest prime number is two")
 else:
-   for i in range(2,int((input_number**0.5)+1)):
-      if(input_number %i == 0):
-         isPrime=False
-         break
-   if(isPrime):
-      print(f"{input_number},it is a prime number.")
-   else:
-      print(f"{input_number},it is not a prime number.")
-      
+    prime_numbers.append(2)
+    for i in range(3,input_number+1,2):
+         for j in range(3,int(i**0.5)+1):
+          if(i %j == 0):
+             break
+         else:
+             prime_numbers.append(i)
+    print(f"{prime_numbers}")
