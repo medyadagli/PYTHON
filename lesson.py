64c10641cc4eab94b5758fe48f262b3f6b693cc1
Medya Dağlı <medyadagli9@gmail.com>
2026-09-21 21:01:58 +0300

    EBOB-EKOK

--- lesson.py
+++ lesson.py
@@ -1,15 +1,12 @@
-input_number=int(input("Enter a number:"))
-prime_numbers=[]
-if input_number<0:
-    print("Entered a negative number.Enter positive a number.")
-elif input_number>0 and input_number<2:
-    print("The smallest prime number is two")
-else:
-    prime_numbers.append(2)
-    for i in range(3,input_number+1,2):
-         for j in range(3,int(i**0.5)+1):
-          if(i %j == 0):
-             break
-         else:
-             prime_numbers.append(i)
-    print(f"{prime_numbers}")
+number1=int(input("Enter the first number:")) #ilk sayıyı giriniz.
+number2=int(input("Enter the second number:")) #2. sayıyı giriniz
+smaller=min(number1,number2) #iki sayıdan küçük olanı alınız.
+
+for i in range(smaller,0,-1): # küçük sayıyı belirledikten sonra sıfıra kadar birer azaltınız.
+    if number1%i == 0 and number2%i ==0: #birinci sayının bölümünden kalan sıfır ve ikinci sayıdan bölümü kalan sıfır 
+        gcd=i #ebobu gösterir.[cisidi]
+        break
+lcm=(number1*number2)//gcd #lcm ekok 
+print(f"The GCD of {number1} and {number2} is {gcd}")
+print(f"The LCM of {number1} and {number2} is {lcm}")
+
